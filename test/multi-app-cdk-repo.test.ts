b9b5db22c80eb0d475db02f77f73dfb3c8fc51b9
Chris Gradwohl <15326707+cgradwohl@users.noreply.github.com>
2023-12-08 14:14:50 -0800

    cdk init

---
+++ test/multi-app-cdk-repo.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MultiAppCdkRepo from '../lib/multi-app-cdk-repo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/multi-app-cdk-repo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MultiAppCdkRepo.MultiAppCdkRepoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
