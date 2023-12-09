e7d8b4a5c7ecd25636d976578d200d9fae6a11c4
Chris Gradwohl <15326707+cgradwohl@users.noreply.github.com>
2023-12-08 16:15:18 -0800

    nice.

--- src/apps/app-1/index.ts
+++ src/apps/app-1/index.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import { MultiAppCdkRepoStack } from "../lib/multi-app-cdk-repo-stack";
+import { MultiAppCdkRepoStack } from "./stacks/multi-app-cdk-repo-stack";
 
 const app = new cdk.App();
 new MultiAppCdkRepoStack(app, "MultiAppCdkRepoStack", {});
--- src/apps/app-2/index.ts
+++ src/apps/app-2/index.ts
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import { MultiAppCdkRepoStack } from "../lib/multi-app-cdk-repo-stack";
+import { MultiAppCdkRepoStack } from "../app-1/stacks/multi-app-cdk-repo-stack";
+import { isEven } from "../../libs/is-even";
 
 const app = new cdk.App();
 new MultiAppCdkRepoStack(app, "MultiAppCdkRepoStack", {});
 
-app.synth();
+if (isEven(2)) {
+  app.synth();
+}
