cfb548110a6317b111253346e40d1f1aaed99747
Chris Gradwohl <15326707+cgradwohl@users.noreply.github.com>
2023-12-11 09:54:15 -0800

    save.

--- src/apps/app-1/index.ts
+++ src/apps/app-1/index.ts
@@ -2,8 +2,9 @@
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { MultiAppCdkRepoStack } from "./stacks/multi-app-cdk-repo-stack";
+import { HelloCdkStack } from "./stacks/hello-stack";
 
 const app = new cdk.App();
 new MultiAppCdkRepoStack(app, "MultiAppCdkRepoStack", {});
-
+new HelloCdkStack(app, "HelloCdkStack", {});
 app.synth();
